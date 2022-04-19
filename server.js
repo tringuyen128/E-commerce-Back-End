1051c7389e2652925932a931bdbf7a3d5cac3c7d
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-19 07:47:40 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+// import sequelize connection
+const sequelize = require('./config/connection')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+})
